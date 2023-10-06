3afba92c09a2c47d8cd0f8a7e9044cb2a503bb95
Alexandre Pereira Nunes <alexandre.nunes@gmail.com>
2023-10-06 16:28:00 -0300

    Attempt to implement reauthentication... the token expires on very long call chains.

--- src/main/kotlin/com/aleques/eduzzApi/EduzzApiProvider.kt
+++ src/main/kotlin/com/aleques/eduzzApi/EduzzApiProvider.kt
@@ -7,10 +7,11 @@ import com.aleques.eduzzApi.util.defaultEduzzApiHttpClientBuilder
 import com.aleques.eduzzApi.util.eduzzSvcRetry
 import com.aleques.eduzzApi.util.setHttpClient
 import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import kotlinx.coroutines.runBlocking
 import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.json.Json
+import okhttp3.*
 import okhttp3.MediaType.Companion.toMediaType
-import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.http.*
 import java.time.LocalDate
@@ -20,7 +21,7 @@ interface EduzzService {
     @FormUrlEncoded
     @POST("credential/generate_token")
     suspend fun authenticate(
-        @Field("email") email: String, @Field("publickey") publickey: String, @Field("apikey") apikey: String
+            @Field("email") email: String, @Field("publickey") publickey: String, @Field("apikey") apikey: String
     ): EduzzAuthResponse
 
     @GET("/user/get_me")
@@ -28,31 +29,31 @@ interface EduzzService {
 
     @GET("/sale/get_sale_list")
     suspend fun listSales(
-        @Header("token") token: String?,
-        @Query("start_date") startDate: LocalDate? = null,
-        @Query("end_date") endDate: LocalDate? = null,
-        @Query("page") page: Int?,
-        @Query("contract_id") cId: Int? = null,
-        @Query("affiliate_id") affId: Int? = null,
-        @Query("content_id") contId: Int? = null,
-        @Query("invoice_status") invStatus: Int? = null,
-        @Query("client_email") email: String? = null,
-        @Query("client_document") cliDoc: String? = null,
-        @Query("date_type") dateType: String? = null,
+            @Header("token") token: String?,
+            @Query("start_date") startDate: LocalDate? = null,
+            @Query("end_date") endDate: LocalDate? = null,
+            @Query("page") page: Int?,
+            @Query("contract_id") cId: Int? = null,
+            @Query("affiliate_id") affId: Int? = null,
+            @Query("content_id") contId: Int? = null,
+            @Query("invoice_status") invStatus: Int? = null,
+            @Query("client_email") email: String? = null,
+            @Query("client_document") cliDoc: String? = null,
+            @Query("date_type") dateType: String? = null,
     ): EduzzGetInvoiceResponse
 
     @GET("/sale/last_days_amount")
     suspend fun getLastDaysAmount(
-        @Header("token") token: String?, @Query("days") howManyDays: Int?
+            @Header("token") token: String?, @Query("days") howManyDays: Int?
     ): EduzzLastDaysAmountResponse
 
     @GET("/financial/statement")
     suspend fun getFinancialStatement(
-        @Header("token") token: String?,
-        @Query("start_date") sd: LocalDate,
-        @Query("end_date") ed: LocalDate,
-        @Query("page") page: Int,
-        @Query("per_page") perPage: Int? = 100
+            @Header("token") token: String?,
+            @Query("start_date") sd: LocalDate,
+            @Query("end_date") ed: LocalDate,
+            @Query("page") page: Int,
+            @Query("per_page") perPage: Int? = 100
     ): EduzzFinancialStatementResponse
 
     @GET("/sale/get_sale/{id}")
@@ -64,23 +65,36 @@ interface EduzzService {
 
     @GET("/fiscal/get_taxdocumentlist")
     suspend fun getTaxDocumentList(
-        @Header("token") token: String?,
-        @Query("start_date") startDate: LocalDate? = null,
-        @Query("end_date") endDate: LocalDate? = null,
-        @Query("page") page: Int? = null,
-        @Query("per_page") perPage: Int? = 100,
-        @Query("document_status") documentStatus: String? = null,
-        @Query("name") name: String? = null,
-        @Query("email") email: String? = null,
-        @Query("sale_id") saleId: Long? = null
+            @Header("token") token: String?,
+            @Query("start_date") startDate: LocalDate? = null,
+            @Query("end_date") endDate: LocalDate? = null,
+            @Query("page") page: Int? = null,
+            @Query("per_page") perPage: Int? = 100,
+            @Query("document_status") documentStatus: String? = null,
+            @Query("name") name: String? = null,
+            @Query("email") email: String? = null,
+            @Query("sale_id") saleId: Long? = null
     ): EduzzGetTaxDocListResponse
+}
+
+private val Response.responseCount: Int
+    get() = generateSequence(this) { it.priorResponse }.count()
+
+class EduzzAuthenticator(private val getToken: () -> String) : Authenticator {
+    override fun authenticate(route: Route?, response: Response): Request? {
+        if (response.responseCount >= 3) {
+            return null
+        }
+        return response.request.newBuilder().header("token", getToken()).build()
+    }
+
 }
 
 @Suppress("unused")
 @ExperimentalSerializationApi
 class EduzzApiProvider(
-    private val eduzzAuthProvider: EduzzAuthData,
-    httpClient: OkHttpClient = defaultEduzzApiHttpClientBuilder.build()
+        private val eduzzAuthProvider: EduzzAuthData,
+        httpClient: OkHttpClient = defaultEduzzApiHttpClientBuilder.build()
 ) {
     companion object {
         private const val EDUZZBASEURL = "https://api2.eduzz.com/"
@@ -94,10 +108,14 @@ class EduzzApiProvider(
     private val json = Json { isLenient = false; ignoreUnknownKeys = false }
 
     private val retrofit = setHttpClient(
-        Retrofit.Builder().baseUrl(EDUZZBASEURL)
+            Retrofit.Builder().baseUrl(EDUZZBASEURL)
 //        .addConverterFactory(GsonConverterFactory.create(gson))
-            .addConverterFactory(json.asConverterFactory(contentType)),
-        httpClient
+                    .addConverterFactory(json.asConverterFactory(contentType)),
+            httpClient.newBuilder().authenticator(EduzzAuthenticator {
+                runBlocking { authenticate() }
+                authToken!!
+            }
+            ).build()
     ).build()
     private val service = retrofit.create(EduzzService::class.java)
     private var authToken: String? = null
@@ -136,15 +154,15 @@ class EduzzApiProvider(
     }
 
     suspend fun getSalesList(
-        startDate: LocalDate,
-        endDate: LocalDate,
-        contractId: Int? = null,
-        affiliateId: Int? = null,
-        contentId: Int? = null,
-        invoiceStatus: Int? = null,
-        clientEmail: String? = null,
-        clientDocument: String? = null,
-        dateType: String? = null
+            startDate: LocalDate,
+            endDate: LocalDate,
+            contractId: Int? = null,
+            affiliateId: Int? = null,
+            contentId: Int? = null,
+            invoiceStatus: Int? = null,
+            clientEmail: String? = null,
+            clientDocument: String? = null,
+            dateType: String? = null
     ): List<EduzzInvoice> {
         checkAuth()
         var done = false
@@ -153,24 +171,25 @@ class EduzzApiProvider(
         do {
             val r = eduzzSvcRetry {
                 service.listSales(
-                    authToken,
-                    startDate,
-                    endDate,
-                    page,
-                    contractId,
-                    affiliateId,
-                    contentId,
-                    invoiceStatus,
-                    clientEmail,
-                    clientDocument,
-                    dateType
+                        authToken,
+                        startDate,
+                        endDate,
+                        page,
+                        contractId,
+                        affiliateId,
+                        contentId,
+                        invoiceStatus,
+                        clientEmail,
+                        clientDocument,
+                        dateType
                 )
             }
 
             page++
             retVal += r.data
-            if (r.paginator.isNullOrEmpty() || page > (r.paginator!!.getOrDefault("totalPages", 1) ?: 1)) done =
-                true
+            if (r.paginator.isNullOrEmpty() || page > (r.paginator!!.getOrDefault("totalPages", 1)
+                            ?: 1)) done =
+                    true
         } while (!done)
         return retVal
     }
@@ -184,7 +203,8 @@ class EduzzApiProvider(
             val r = eduzzSvcRetry { service.getFinancialStatement(authToken, startDate, endDate, page) }
             page++
             retVal += r.data
-            if (r.paginator.isNullOrEmpty() || page > (r.paginator!!.getOrDefault("totalPages", 1) ?: 1)) done = true
+            if (r.paginator.isNullOrEmpty() || page > (r.paginator!!.getOrDefault("totalPages", 1)
+                            ?: 1)) done = true
         } while (!done)
         return retVal
     }
@@ -195,12 +215,12 @@ class EduzzApiProvider(
     }
 
     suspend fun getTaxDocList(
-        startDate: LocalDate? = null,
-        endDate: LocalDate? = null,
-        documentStatus: String? = null,
-        name: String? = null,
-        email: String? = null,
-        saleId: Long? = null
+            startDate: LocalDate? = null,
+            endDate: LocalDate? = null,
+            documentStatus: String? = null,
+            name: String? = null,
+            email: String? = null,
+            saleId: Long? = null
     ): List<EduzzTaxDoc> {
         checkAuth()
         var done = false
@@ -209,12 +229,13 @@ class EduzzApiProvider(
         do {
             val r = eduzzSvcRetry {
                 service.getTaxDocumentList(
-                    authToken, startDate, endDate, page, 100, documentStatus, name, email, saleId
+                        authToken, startDate, endDate, page, 100, documentStatus, name, email, saleId
                 )
             }
             page++
             retVal += r.data
-            if (r.paginator.isNullOrEmpty() || page > (r.paginator!!.getOrDefault("totalPages", 1) ?: 1)) {
+            if (r.paginator.isNullOrEmpty() || page > (r.paginator!!.getOrDefault("totalPages", 1)
+                            ?: 1)) {
                 done = true
             }
         } while (!done)
--- src/main/kotlin/com/aleques/eduzzApi/util/DefaultHttpClient.kt
+++ src/main/kotlin/com/aleques/eduzzApi/util/DefaultHttpClient.kt
@@ -4,12 +4,14 @@ import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import java.util.concurrent.TimeUnit
 
-internal val defaultEduzzApiHttpClientBuilder = OkHttpClient.Builder().connectTimeout(50, TimeUnit.SECONDS)
-    .writeTimeout(50, TimeUnit.SECONDS).readTimeout(50, TimeUnit.SECONDS)
+internal val defaultEduzzApiHttpClientBuilder = OkHttpClient.Builder()
+        .connectTimeout(50, TimeUnit.SECONDS)
+        .writeTimeout(50, TimeUnit.SECONDS)
+        .readTimeout(50, TimeUnit.SECONDS)
 
 internal fun setHttpClient(
-    builder: Retrofit.Builder,
-    httpClient: OkHttpClient
+        builder: Retrofit.Builder,
+        httpClient: OkHttpClient
 ): Retrofit.Builder {
     return builder.client(httpClient)
 }
